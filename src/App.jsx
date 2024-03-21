f04d47c369220273ac6d134c0abbdeacdf0b1d77
SanthoshToorpu <90833739+SanthoshToorpu@users.noreply.github.com>
2024-03-21 22:52:07 +0530

    Memoization using react memo

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,43 @@
-import {React, useState} from 'react'
+import React, {useState} from 'react'
 
 
 const App = () => {
 
-  const [title, setTitle] = useState('Initial Number')
+  return (
+    <div>
+      <h1>Here Static components of a function ehich means the functional components whose props arent being changed are being re-rendereed</h1>
+      <br />
+      <br />
+      <h1>We will be using react.memo to prevent rerender</h1>
+      <Header title="Here the below header will be updated when a button is clicked"></Header>
+      <HeaderwithButton title="Hallow No number"></HeaderwithButton>
+
+    </div>
+  )
+}
+
+const HeaderwithButton = (props)=>{
+
+  const [title, setTitle] = useState(props.title)
 
   const updateTitle = () => {
     setTitle(Math.random()*10000)
   }
 
-  return (
-    <div>
-      <h1>Create a button such that whwn clicked the header is updated with some random number</h1>
-      <Header title="Here the below header will be updated when a button is clicked"></Header>
-      <button onClick={updateTitle}>Update Header</button>
-      <Header title={title}></Header>
 
+  return(
+    <div style={{display:"flex", flexDirection:"column"}}>
+      <button style={{maxWidth:"200px"}} onClick={updateTitle}>Update Header</button>
+      {title}
     </div>
   )
 }
-
-const Header = (props) => {
+const Header = React.memo((props) => {
   return(
     <div>
       {props.title}
     </div>
   )
-}
+})
 
 export default App
