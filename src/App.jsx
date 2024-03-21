1e3f86032d17573c3873e31b1be0113b1b1eba99
SanthoshToorpu <90833739+SanthoshToorpu@users.noreply.github.com>
2024-03-21 22:24:10 +0530

    updated header with a function

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      
+      <Header title="Welcome to My New Assignment"></Header>
+    </div>
+  )
+}
+
+const Header = (props) => {
+  return (
+    <div>
+      {props.title}
     </div>
   )
 }
