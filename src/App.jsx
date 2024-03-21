2c82257a38ed36c83ce8b428613240db913f49a0
SanthoshToorpu <90833739+SanthoshToorpu@users.noreply.github.com>
2024-03-21 22:32:33 +0530

    Header With Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
+import {React, useState} from 'react'
+
 
 const App = () => {
+
+  const [title, setTitle] = useState('Initial Number')
+
+  const updateTitle = () => {
+    setTitle(Math.random()*10000)
+  }
+
   return (
     <div>
-      <Header title="Welcome to My New Assignment"></Header>
+      <h1>Create a button such that whwn clicked the header is updated with some random number</h1>
+      <Header title="Here the below header will be updated when a button is clicked"></Header>
+      <button onClick={updateTitle}>Update Header</button>
+      <Header title={title}></Header>
+
     </div>
   )
 }
 
 const Header = (props) => {
-  return (
+  return(
     <div>
       {props.title}
     </div>
